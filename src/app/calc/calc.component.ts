133b91afb8cfc794e785d0a9ff194f3f17a57d85
LuisHenrique <luishenriquecorporativo@gamil.com>
2023-04-18 00:32:28 -0300

    Calculadora

---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styleUrls: ['./calc.component.css']
+})
+export class CalcComponent {
+  num1 : number = 0;
+  num2 : number = 0;
+  result : number = 0;
+
+  calc(){
+    this.result = this.num1 + this.num2;
+  }
+
+  calc2(){
+    this.result = this.num1 - this.num2;
+  }
+
+  calc3(){
+    this.result = this.num1 * this.num2;
+  }
+
+  calc4(){
+    this.result = this.num1 / this.num2;
+  }
+
+
+
+}
